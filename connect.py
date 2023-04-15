0a1027417b6e9964b1183ebacce1130a1ffae97b
danielbaniel-bunner <dlaufer_111@hotmail.com>
2023-04-15 17:40:25 +0300

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+url = "https://api.openai.com/v1/chat/completions"
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer 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"
+}
+while(1):
+    input = input()
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": input }],
+        "temperature": 0.7
+    }
+
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    json_data = response.json()
+    message = json_data['choices'][0]['message']['content']
+    if response.status_code == 200:
+        print(message)
+    else:
+        print(f"Error: {response.status_code}")
